24633d8b4c070e5f2a9e16143acbc58aa457cf5b
general_lee <l.georgelee98@hotmail.com>
2020-10-11 19:38:27 +0100

    Made header generic

--- root/js/header.js
+++ root/js/header.js
@@ -1,4 +1,5 @@
-document.write(
+window.addEventListener("load", function(){
+	document.querySelector('#header').insertAdjacentHTML('beforebegin',
 	'<ul>' +
 	 ' <li><a class="active" href="index.html">Home</a></li>' +
 	 ' <li><a href="new.html">New aircraft</a></li>' +
@@ -7,4 +8,5 @@ document.write(
 	 ' <li><a href="reference.html">References</a></li>' +
 	 ' <li><a href="contact.html">Contact us</a></li>' +
 	'</ul>'
-);
+	);
+});
