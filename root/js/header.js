41d7db9f45dcfef4186d4548cf1a7dadb8d8e53f
general_lee <l.georgelee98@hotmail.com>
2020-10-14 13:43:41 +0100

    Work on header

--- root/js/header.js
+++ root/js/header.js
@@ -2,7 +2,6 @@ window.addEventListener("load", function(){
 	document.querySelector('#header').insertAdjacentHTML('beforebegin',
 	'<ul>' +
 	 ' <li><a class="active" href="index.html">Home</a></li>' +
-	 ' <li><a href="new.html">New aircraft</a></li>' +
 	 ' <li><a href="current.html">Current aircraft</a></li>' +
 	 ' <li><a href="old.html">Old aircraft</a></li>' +
 	 ' <li><a href="reference.html">References</a></li>' +
