43f018cb05bae4c325034d9020cb64eced0e4d47
general_lee <l.georgelee98@hotmail.com>
2020-10-23 00:20:02 +0100

    Fixed js issue and added a contact page

--- root/js/header.js
+++ root/js/header.js
@@ -1,18 +1,18 @@
 window.addEventListener("load", function(){
-	document.querySelector('#header').insertAdjacentHTML('beforebegin',
+	document.getElementById('header').innerHTML +=
 	'<ul>' +
-	 ' <li><a class="active" href="index.html">Home</a></li>' +
+	 ' <li><a href="index.html">Home</a></li>' +
 	 ' <li><a href="current.html">Current aircraft</a></li>' +
 	 ' <li><a href="old.html">Old aircraft</a></li>' +
 	 ' <li><a href="reference.html">References</a></li>' +
 	 ' <li><a href="contact.html">Contact us</a></li>' +
 	'</ul>'
-	);
+	;
 	
-	document.querySelector('#footer').insertAdjacentHTML('beforebegin',
+	document.getElementById('footer').innerHTML +=
 		'<footer>' +
 		  '<p>Author: George Lee<br>' +
 		  '<a href="glee@example.com">glee@example.com</a></p>' +
 		'</footer>'
-	);
+	;
 });
